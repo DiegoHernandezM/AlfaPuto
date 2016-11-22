65d39ccef6d683ee97a27dbe6f017e1aa3231a85
AlvaroCarrillo1 <alvarocarrillo351@yahoo.com>
2016-11-21 21:31:02 -0600

    'Initial'

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
@@ -64,7 +64,34 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],
+        ],*/
+
+        // on fortrabbit: construct credentials from App secrets
+if (isset($_SERVER['APP_SECRETS'])) {
+    $secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);
+    $mysql = [
+        'driver'    => 'mysql',
+        'host'      => $secrets['MYSQL']['HOST'],
+        'port'      => $secrets['MYSQL']['PORT'],
+        'database'  => $secrets['MYSQL']['DATABASE'],
+        'username'  => $secrets['MYSQL']['USER'],
+        'password'  => $secrets['MYSQL']['PASSWORD'],
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+        'strict'    => false,
+    ];
+}
+
+return [
+    'fetch'         => PDO::FETCH_OBJ,
+    'default'       => env('DB_CONNECTION', 'mysql'),
+    'connections'   => [
+        'mysql' => $mysql,
+    ],
+    'migrations' => 'migrations'
+    // possible other code …
+];
 
         'pgsql' => [
             'driver' => 'pgsql',
